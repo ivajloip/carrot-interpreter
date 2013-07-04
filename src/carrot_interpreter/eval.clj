1ed2334af9fe6b996acb0868574cff822aa2739b
Ivaylo Petrov <ivajloip@gmail.com>
2013-07-05 01:23:12 +0300

    Moving forward

--- src/carrot_interpreter/eval.clj
+++ src/carrot_interpreter/eval.clj
@@ -1,6 +1,6 @@
 (ns carrot-interpreter.eval)
 
-(defn default-env [] '())
+(declare evaluate)
 
 (defn error
   [& args]
@@ -12,13 +12,64 @@
         parent (recur parent var-name)
         :else (error "Unbound variable: " var-name)))
 
+(defn empty-env
+  []
+  {:bindings (atom {})
+   :parent nil})
+
+(defn extend-env
+  [bindings parent]
+  {:bindings (atom bindings) :parent parent})
+
+(defn primitive
+  [func]
+  {:kind :primitive :code func})
+
+(defn default-env
+  []
+  (extend-env {'print (primitive prn)
+               'twice (primitive (fn [a] (+ a a)))
+               '+ (primitive +)
+               '* (primitive *)
+               '== (primitive =)
+               '- (primitive -)}
+              (empty-env)))
+
+(defn type? 
+  [ast tag]
+  (and (seq? ast)
+       (= tag (first ast))))
+
+(defn modify
+  [original-env var-name value]
+  (loop [{:keys (bindings parent)} original-env]
+    (cond (find @bindings var-name) (swap! bindings assoc var-name value)
+          parent (recur parent)
+          :else (swap! (:bindings original-env) assoc var-name value))))
+
+(defn make-function 
+  [[_ function-name args & body] env]
+  (modify env 
+          function-name
+          {:kind :function
+           :args args
+           :body (list* 'begin body)
+           :env env})
+  (str "#'" function-name))
+
+(defn eval-set
+  [[_ var-name expr] env]
+  (modify env var-name (evaluate expr env))
+  nil)
+
 (defn evaluate
   [ast env]
   (cond (number? ast) ast
         (symbol? ast) (lookup env ast)
+        (type? ast 'function) (make-function ast env)
+        (type? ast 'set!) (eval-set ast env)
         :else ast
         ;(tag? ast '*) (lift * ast env)
-        ;(tag? ast 'set!) (eval-set ast env)
         ;(tag? ast 'if) (eval-if ast env)
 
         ;(tag? ast 'begin) (eval-begin ast env)
--- src/carrot_interpreter/parser.clj
+++ src/carrot_interpreter/parser.clj
@@ -1,6 +1,6 @@
 (ns carrot-interpreter.parser
   (:use (blancas.kern (core :exclude (parse) :as kern))
-        blancas.kern.expr
+        ;blancas.kern.expr
         carrot-interpreter.lexer))
 
 (declare condition)
@@ -57,29 +57,69 @@
    "floor" #(Math/floor %) "deg"  #(Math/toDegrees %) "rad"  #(Math/toRadians %)})
 
 (def funcall
-"This parser corresponds a syntax rule: FUN := ID (expr)
- It returns the result of applying the primitive function to the
- result of the expression."
-  (bind [id identifier ex (parens expr)]
-      (return ((fun-tbl id) ex))))
+  (bind [func (<|> (fwd var-ref) (parens (fwd expr)))
+         exprs (parens (sep-by comma expr))]
+      (return (list* func exprs))))
+;"This parser corresponds a syntax rule: FUN := ID (expr)
+; It returns the result of applying the primitive function to the
+; result of the expression."
+;  (bind [id identifier ex (parens expr)]
+;      (return ((fun-tbl id) ex))))
 
 (def factor
   "Evaluates a number (double), a call to a buil-in function, or an
    expression in parens to (for example) change the order of evaluation."
-  (<|> float-lit funcall (parens (fwd expr))))
+  (<|> float-lit (<:> funcall) var-ref (parens (fwd expr))))
+
+(defn prefix1
+  [p op]
+  (<|> (bind [f op
+              a (prefix1 p op)]
+          (return (list f a)))
+       (bind [a p] (return a))))
+
+(defn chainl1
+  [p op]
+  (letfn [(rest [a] (<|> (bind [f op b p] (rest (list f a b)))
+                         (return a)))]
+    (bind [a p] (rest a))))
+
+(defn chainr1
+  [p op]
+  (bind [a p]
+        (<|> (bind [f op b (chainr1 p op)]
+                   (return (list f a b)))
+             (return a))))
+
+(defn ops
+  [& symbols]
+  (bind [op (apply token symbols)]
+    (return (symbol op))))
+
+(defn rename
+  [op n]
+  (>> (token op) (return n)))
 
 ;; Chain calls parse multiple occurrences of a kind of operator
 ;; working on operands that may be expressions. In this case,
 ;; uni-op operators have the highest precedence while the and-op
 ;; operator has the lowest.
 
-(def unary (prefix1 factor uni-op))  ;; -(10), !(3>0)
-(def power (chainr1 unary  pow-op))  ;; 2^32
-(def term  (chainl1 power  mul-op))  ;; 3 * 34 * ...
-(def sum   (chainl1 term   add-op))  ;; 5 + 2*3 + ...
-(def relex (chainl1 sum    rel-op))  ;; sin(0.5) > 0
-(def orex  (chainl1 relex  or-op))   ;; sqrt(10) > 0 || tan(0) == 1 || ...
-(def expr  (chainl1 orex   and-op))  ;; sqrt(10) > 0 && tan(0) == 1 || sin(0) > 0 & ...
+;(def unary (prefix1 factor uni-op))  ;; -(10), !(3>0)
+;(def power (chainr1 unary  pow-op))  ;; 2^32
+;(def term  (chainl1 power  mul-op))  ;; 3 * 34 * ...
+;(def sum   (chainl1 term   add-op))  ;; 5 + 2*3 + ...
+;(def relex (chainl1 sum    rel-op))  ;; sin(0.5) > 0
+;(def orex  (chainl1 relex  or-op))   ;; sqrt(10) > 0 || tan(0) == 1 || ...
+;(def expr  (chainl1 orex   and-op))  ;; sqrt(10) > 0 && tan(0) == 1 || sin(0) > 0 & ...
+
+(def unary (prefix1 factor (ops "!" "-")))
+(def power (chainr1 unary  (ops "^")))
+(def term  (chainl1 power  (ops "*" "/" "%")))
+(def sum   (chainl1 term   (ops "+" "-")))
+(def relex (chainl1 sum    (ops "==" "!=" ">=" "<=" ">" "<")))
+(def orex  (chainl1 relex  (rename "&&" 'and)))
+(def expr  (chainl1 orex   (rename "||" 'or)))
 
 
 
