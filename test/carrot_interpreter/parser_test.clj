f4cf8d770dba16d0d5e355598fa215468784613e
Ivaylo Petrov <ivajloip@gmail.com>
2013-07-09 08:45:27 +0300

    Adding some tests

--- test/carrot_interpreter/parser_test.clj
+++ test/carrot_interpreter/parser_test.clj
@@ -37,10 +37,10 @@
     (is (= (:value (kern/parse unary "-2")) '(- 2.0)))))
 
 ; -------------- power -------------------
-;(deftest unary-parses-negation
-;  (testing "Parsing a power expression works"
-;    (is (= (:value (kern/parse power "3^2"))
-;           '(^ 3.0 2.0)))))
+(deftest power-parses-power
+  (testing "Parsing a power expression works"
+    (is (= (:value (kern/parse power "3^2"))
+           (list (symbol "^") 3.0 2.0)))))
 
 ; -------------- term -------------------
 (deftest term-parses-multiplication
@@ -63,3 +63,66 @@
 (deftest term-parses-subtraciton
   (testing "Parsing a single subtraction works"
     (is (= (:value (kern/parse sum "2 - 3")) '(- 2.0 3.0)))))
+
+; -------------- relax -------------------
+(deftest relax-parses-equation
+  (testing "Parsing a comparison expression =="
+    (is (= (:value (kern/parse relex "2 == x")) '(== 2.0 x)))))
+
+(deftest relax-parses-inequality
+  (testing "Parsing a comparison expression !="
+    (is (= (:value (kern/parse relex "2 != x")) '(!= 2.0 x)))))
+
+(deftest relax-parses-less-or-equal
+  (testing "Parsing a comparison expression <="
+    (is (= (:value (kern/parse relex "2 <= x")) '(<= 2.0 x)))))
+
+(deftest relax-parses-greater-or-equal
+  (testing "Parsing a comparison expression >="
+    (is (= (:value (kern/parse relex "2 >= x")) '(>= 2.0 x)))))
+
+(deftest relax-parses-less
+  (testing "Parsing a comparison expression <"
+    (is (= (:value (kern/parse relex "2 < x")) '(< 2.0 x)))))
+
+(deftest relax-parses-greater
+  (testing "Parsing a comparison expression >"
+    (is (= (:value (kern/parse relex "2 > x")) '(> 2.0 x)))))
+
+; -------------- orex -------------------
+(deftest orex-parses-and
+  (testing "Parsing a comparison expression &&"
+    (is (= (:value (kern/parse orex "y && x")) '(and y x)))))
+
+; -------------- expr -------------------
+(deftest expr-parses-or
+  (testing "Parsing a comparison expression ||"
+    (is (= (:value (kern/parse expr "y || x")) '(or y x)))))
+
+(deftest expr-parses-or
+  (testing "Parsing an expression with different priorities"
+    (is (= (:value (kern/parse expr "y || x && z == 3 + 4 * -2"))
+           '(or y (and x (== z (+ 3.0 (* 4.0 (- 2.0))))))))))
+
+; -------------- assign -------------------
+(deftest assign-parses-assigning
+  (testing "Parsing an assigning"
+    (is (= (:value (kern/parse assign "y = x")) '(set! y x)))))
+
+; -------------- var-ref -------------------
+(deftest var-ref-parses-var-references
+  (testing "Parsing a variable references"
+    (is (= (:value (kern/parse var-ref "my_variable")) 'my_variable))))
+
+; -------------- block -------------------
+(deftest block-parses-simple-code-block
+  (testing "Parsing a simple code block"
+    (is (= (:value (kern/parse block "do 3 end")) '(3.0)))))
+
+(deftest block-parses-code-block-with-multiple-statements
+  (testing "Parsing a code block with multipliple statemets"
+    (is (= (:value (kern/parse block "do 3 \n4 end")) '(3.0 4.0)))))
+
+(deftest block-parses-nested-code-block
+  (testing "Parsing a nested code block"
+    (is (= (:value (kern/parse block "do do 3 end\n4 end")) '((3.0) 4.0)))))
