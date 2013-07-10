ff513dcf1d479c0f82bc447df1de0702055131f9
Ivaylo Petrov <ivajloip@gmail.com>
2013-07-10 18:38:27 +0300

    Adding eval tests

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.runa/conjure "2.1.3"]
                  [org.blancas/kern "0.7.0"]]
   :main carrot-interpreter.core)
 
--- test/carrot_interpreter/eval_test.clj
+++ test/carrot_interpreter/eval_test.clj
@@ -1,7 +1,62 @@
 (ns carrot-interpreter.eval-test
     (:use clojure.test
+          conjure.core
           carrot-interpreter.eval))
 
-(deftest a-test
-    (testing "FIXME, I fail."
-          (is (= 1 1))))
+; -------------- eval-set -------------------
+
+(deftest eval-set-evaluates-variable-assigning
+  (testing "Eval-set calls modify with the right arguments"
+    (stubbing [modify :foo evaluate :var]
+              (eval-set [:x 'bar :var] {})
+              (verify-call-times-for modify 1)
+              (verify-call-times-for evaluate 1)
+              (verify-first-call-args-for evaluate :var {})
+              (verify-first-call-args-for modify {} 'bar :var))))
+
+; -------------- empty-env -------------------
+
+(deftest empty-env-creates-empty-entironment
+  (testing "Empty-env creates an empty environment with no parent"
+    (let [empty-environment (empty-env)]
+      (are [expected actual]
+           (= expected actual)
+           {} (deref (:bindings empty-environment)) 
+           nil (:parent empty-environment)))))
+
+; -------------- type? -------------------
+
+(deftest type?-recognizes-the-type
+  (testing "Type? recognizes correctly function"
+    (are [value] 
+         value
+         (type? '(function func () 3.0) 'function)
+         (type? '(set! x y) 'set!))))
+
+; -------------- error -------------------
+
+(deftest error-throws-error
+  (testing (str "Error method throws the right type of exception" 
+                " with the right message")
+    (is (thrown-with-msg? RuntimeException
+                          #"Error Some message"
+                          (error "Error" " Some message")))))
+
+
+; -------------- lookup -------------------
+
+(deftest lookup-find-var-in-current-env
+  (testing "Lookup can find some variable in the current environment"
+    (let [env {:bindings (atom {'x 3}) :parent nil}]
+      (is (= (lookup env 'x) 3))))
+  (testing "Lookup can find some variable in the parent environment"
+    (let [env {:bindings (atom {'x 3})
+               :parent {:bindings (atom {'y 4}) :parent nil}}]
+      (is (= (lookup env 'y) 4))))
+  (testing "Lookup throws error on fail"
+    (let [env {:bindings (atom {}) :parent nil}]
+      (mocking [error]
+               (lookup env 'x)
+               (verify-call-times-for error 1)
+               (verify-first-call-args-for error "Unbound variable: " 'x)))))
+
